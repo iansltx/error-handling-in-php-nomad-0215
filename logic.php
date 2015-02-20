1f843b8f03a3b1ad767895bbb802487c894510b0
Ian Littman <iansltx+p@gmail.com>
2015-02-19 22:27:01 -0600

    Make line 7 do E_NOTICEs again

--- logic.php
+++ logic.php
@@ -3,8 +3,8 @@
 $process = function($get, $post, $server) {
     $response = '<h1>Hey ' . (isset($get['name']) ? $get['name'] : 'dude') . '</h1>';
 
-    // raises E_NOTICE
-    // $response = '<h1>Hey ' . (isset($get['name']) ? $get['name'] : 'dude') . '</h1>';
+    // raises E_NOTICE when you use ?name= rather than ?first_name=
+    // $response = '<h1>Hey ' . ($get['first_name'] ?: 'dude') . '</h1>';
 
     // throws exception
     //if ($get['name'] === 'Bobby Tables')
